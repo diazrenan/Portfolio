a16c7c5b84a3e92597fbf24ceb60d5ac02320ac0
diazrenan <rdrenandiaz@gmail.com>
2025-05-10 15:31:41 -0300

    Delete Projeto_Julia directory

--- Projeto_Julia/botao.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("meuBotao").addEventListener("click", function () {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
-    });
-});
